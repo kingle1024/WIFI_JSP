9cda3c4d20cd6835432feaa1c1872fc1124d6bc5
kingle1024 <teran1024@naver.com>
2022-07-23 21:58:43 +0900

    WifiService.java - Too many Connection 해결. - for문 안에서 connection을 선언하게 되면 계속해서 conneciton이 생성되기 때문에 Too many Connection이 발생했었음. - distance() 을 활용하여 x,y좌표의 거리를 구해줌

--- src/main/java/service/WifiService.java
+++ src/main/java/service/WifiService.java
@@ -1,15 +1,19 @@
-package db;
+package service;
+
+import db.Wifi;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class WifiService {
     private String url = "jdbc:mariadb://localhost:3306/wifi";
     private String dbUserId = "root";
     private String dbPassword = "1234";
-
-    public List<Wifi> list(){
+    private static Logger logger = Logger.getLogger(WifiService.class.getName());
+    public List<Wifi> list(HashMap<String, String> map){
         List<Wifi> wifiList = new ArrayList<>();
 
         try {
@@ -21,7 +25,6 @@ public class WifiService {
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
 
-
         try {
             connection = DriverManager.getConnection(url, dbUserId, dbPassword);
             String sql = "select * " +
@@ -32,6 +35,10 @@ public class WifiService {
 
             while(rs.next()){
                 Wifi wifi = new Wifi();
+                String LNT = String.valueOf(rs.getString("LNT")).replace("\"", "");
+                String LAT = String.valueOf(rs.getString("LAT")).replace("\"", "");
+//                wifi.setKm(distance(Double.parseDouble(map.get("LAT")), Double.parseDouble(map.get("LNT")), Double.parseDouble(LNT), Double.parseDouble(LAT), "kilometer"));
+                wifi.setKm(Double.parseDouble(distance(Double.parseDouble(map.get("LAT")), Double.parseDouble(map.get("LNT")), Double.parseDouble(LNT), Double.parseDouble(LAT), "kilometer")));
                 wifi.setX_SWIFI_MGR_NO(rs.getString("X_SWIFI_MGR_NO"));
                 wifi.setX_SWIFI_WRDOFC(rs.getString("X_SWIFI_WRDOFC"));
                 wifi.setX_SWIFI_MAIN_NM(rs.getString("X_SWIFI_MAIN_NM"));
@@ -45,8 +52,8 @@ public class WifiService {
                 wifi.setX_SWIFI_CNSTC_YEAR(rs.getString("X_SWIFI_CNSTC_YEAR"));
                 wifi.setX_SWIFI_INOUT_DOOR(rs.getString("X_SWIFI_INOUT_DOOR"));
                 wifi.setX_SWIFI_REMARS3(rs.getString("X_SWIFI_REMARS3"));
-                wifi.setLAT(rs.getString("LAT"));
-                wifi.setLNT(rs.getString("LNT"));
+                wifi.setLAT(LAT);
+                wifi.setLNT(LNT);
                 wifi.setWORK_DTTM(rs.getString("WORK_DTTM"));
 
                 wifiList.add(wifi);
@@ -78,7 +85,29 @@ public class WifiService {
         }
         return wifiList;
     }
-    public void register(List<Wifi> wifiList){
+    static String distance(double lat1, double lon1, double lat2, double lon2, String unit){
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        if (unit == "kilometer") {
+            dist = dist * 1.609344;
+        } else if(unit == "meter"){
+            dist = dist * 1609.344;
+        }
+
+        return String.valueOf(dist);
+    }
+    static double deg2rad(double deg){
+        return (deg * Math.PI / 180.0);
+    }
+    static double rad2deg(double rad){
+        return (rad * 180 / Math.PI);
+    }
+    public void register(List<Wifi> wifiList) {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -86,13 +115,12 @@ public class WifiService {
         }
         Connection connection = null;
         PreparedStatement preparedStatement = null;
-        ResultSet rs = null;
-
+//        connection.setAutoCommit(false);
         try {
+            connection = DriverManager.getConnection(url, dbUserId, dbPassword);
             for(int i=0; i<wifiList.size(); i++) {
                 Wifi wifi = new Wifi();
                 wifi = wifiList.get(i);
-                connection = DriverManager.getConnection(url, dbUserId, dbPassword);
                 String sql = "insert into data(X_SWIFI_MGR_NO, X_SWIFI_WRDOFC, X_SWIFI_MAIN_NM, X_SWIFI_ADRES1, X_SWIFI_ADRES2," +
                         " X_SWIFI_INSTL_FLOOR, X_SWIFI_INSTL_TY, X_SWIFI_INSTL_MBY, X_SWIFI_SVC_SE, X_SWIFI_CMCWR, " +
                         "X_SWIFI_CNSTC_YEAR, X_SWIFI_INOUT_DOOR, X_SWIFI_REMARS3, LAT, LNT, WORK_DTTM)" +
@@ -113,40 +141,37 @@ public class WifiService {
                 preparedStatement.setString(11, wifi.getX_SWIFI_CNSTC_YEAR());
                 preparedStatement.setString(12, wifi.getX_SWIFI_INOUT_DOOR());
                 preparedStatement.setString(13, wifi.getX_SWIFI_REMARS3());
-                preparedStatement.setString(14, wifi.getLAT());
-                preparedStatement.setString(15, wifi.getLNT());
+                preparedStatement.setString(14, wifi.getLAT().replace("\"", ""));
+                preparedStatement.setString(15, wifi.getLNT().replace("\"", ""));
                 preparedStatement.setString(16, wifi.getWORK_DTTM());
-
                 int affected = preparedStatement.executeUpdate();
+//                connection.commit();
                 if (affected > 0) {
                     System.out.println("저장 성공");
                 } else {
                     System.out.println("저장 실패");
                 }
             }
-
         } catch (SQLException e) {
+            System.out.println("runtime err :"+e);
             throw new RuntimeException(e);
         }finally{
             try {
-                if(rs != null && !rs.isClosed()){
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
                 if(preparedStatement != null && !preparedStatement.isClosed()){
+                    System.out.println("prepared close");
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
+                System.out.println("pre err" + e);
                 throw new RuntimeException(e);
             }
             try {
                 if(connection != null && !connection.isClosed()){
+                    System.out.println("connected close");
                     connection.close();
                 }
             } catch (SQLException e) {
+                System.out.println("conn err " +e);
                 throw new RuntimeException(e);
             }
         }
