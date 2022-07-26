591026e7813d319e31cf9b4a5a5fd85e9ed487ec
kingle1024 <teran1024@naver.com>
2022-07-26 22:34:31 +0900

    fixes #5 [GetWifiList.java] - 정렬된 wifilist에서 20개만 따로 추출하여 gson으로 return

--- src/main/java/servlet/GetWifiList.java
+++ src/main/java/servlet/GetWifiList.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -38,11 +39,15 @@ public class GetWifiList extends HttpServlet {
                 return Double.compare(o2.getKm(), o1.getKm()) == 1 ? -1 : Double.compare(o2.getKm(), o1.getKm()) == 0 ? 0 : 1;
             }
         });
+        List<Wifi> resultWifiList = new ArrayList<>();
+        for(int i=0; i<20; i++){
+            resultWifiList.add(wifilist.get(i));
+        }
         Gson gson = new Gson();
 
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
-        out.println(gson.toJson(wifilist));
+        out.println(gson.toJson(resultWifiList));
     }
 
     public void destroy() {
