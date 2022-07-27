d74b282da917afa86d067af548632df68ce8cb17
kingle1024 <teran1024@naver.com>
2022-07-27 09:01:56 +0900

    불필요한 코드 제거

--- src/main/java/servlet/DeleteHistory.java
+++ src/main/java/servlet/DeleteHistory.java
@@ -1,19 +1,12 @@
 package servlet;
 
-import com.google.gson.Gson;
-import db.Wifi;
 import service.HistoryService;
-import service.WifiService;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
 
 @WebServlet(name ="deleteHistory", value="/deleteHistory")
 public class DeleteHistory extends HttpServlet {
